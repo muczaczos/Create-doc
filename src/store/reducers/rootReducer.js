2fc00d3bc87c47123b1d063fa1e4b2714abb85b4
Arek Szatkowski <muczaczos@gmail.com>
2020-07-17 16:58:26 +0200

    Read state in CreateDocument component

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,16 +1,16 @@
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
+import documentReducer from './documentReducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
 import { firebaseReducer } from 'react-redux-firebase';
-import { documentRReducer } from './documentReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer,
-  gon: documentRReducer,
+  gon: documentReducer,
 });
 
 export default rootReducer;
